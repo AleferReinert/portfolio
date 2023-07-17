cbdcbfd6c8158dc808bed5c25c4b47ad3dc3d76e
Alefer Reinert <aleferreinert@gmail.com>
2023-07-17 16:19:37 -0300

    Return to google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,16 @@ export default class MyDocument extends Document {
       <Html lang='pt-BR'>
         <Head>
           <link rel='icon' href='/favicon.png' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='anonymous'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;800&display=swap'
+            rel='stylesheet'
+          />
         </Head>
         <body>
           <Main />
