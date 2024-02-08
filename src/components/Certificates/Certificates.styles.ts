4cef71ec06d8699c7dda2389a9cfc3605afb9c2b
Alefer Reinert <aleferreinert@gmail.com>
2024-02-08 16:15:07 -0300

    Update Certificates.styles.ts

--- src/components/Certificates/Certificates.styles.ts
+++ src/components/Certificates/Certificates.styles.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   gap: var(--spacing-large);
 
-  @media (min-width: ${breakpoints.large}) {
+  @media (min-width: ${breakpoints.medium}) {
     display: grid;
     grid-template-columns: repeat(2, max-content);
     justify-content: space-between;
