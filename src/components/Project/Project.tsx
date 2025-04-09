36ad57834414800402c415017e55952958ce42f9
Alefer Reinert <aleferreinert@gmail.com>
2025-04-09 17:08:04 -0300

    refactor(Project): wrap skills and remove sizes from image

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -52,18 +52,7 @@ export function Project({
         target='_blank'
         className='relative w-full h-min aspect-25/17 max-w-[280px] md:max-w-[240px] lg:max-w-[280px]'
       >
-        <Image
-          src={img}
-          alt='Imagem do projeto'
-          priority={lazy}
-          fill
-          quality={100}
-          sizes='(min-width: 320px) 240px,
-					(min-width: 576px) 280px,
-					(min-width: 768px) 240px,
-					(min-width: 1024px) 280px,
-					288px'
-        />
+        <Image src={img} alt='Imagem do projeto' priority={lazy} quality={100} width={288} height={196} />
       </a>
       <div className='flex flex-col shrink items-center gap-4 mx-auto w-full tex-center md:text-left'>
         <h3 className='w-full flex justify-between items-baseline'>
@@ -73,7 +62,7 @@ export function Project({
         <p aria-label='descrição' className='leading-5 w-full'>
           {description}
         </p>
-        <ul className='w-full text-primary-theme font-normal gap-1 flex' title='Tecnologias utilizadas'>
+        <ul className='w-full text-primary-theme font-normal gap-1 flex flex-wrap' title='Tecnologias utilizadas'>
           {skills.map((skill) => (
             <li key={skill}>{skill}</li>
           ))}
