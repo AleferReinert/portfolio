f37d7a27956b80b098e498eff8cf75f6ece88f91
Alefer Reinert <aleferreinert@gmail.com>
2025-06-19 13:49:11 -0300

    chore(next.config.js): set deviceSizes to images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    deviceSizes: [256, 280, 384, 420, 560, 640, 750, 828, 1080, 1200, 1920, 2048, 3840]
+  }
+}
 
 module.exports = nextConfig
